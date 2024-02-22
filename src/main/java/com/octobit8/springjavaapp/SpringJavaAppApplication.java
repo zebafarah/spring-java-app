b3d70e975323b27a2954a3221c6a0ca2e0d93eff
zebafarah <zebafarah85@gmail.com>
2024-02-23 00:56:58 +0530

    Initial version

---
+++ src/main/java/com/octobit8/springjavaapp/SpringJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.springjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octobit8/springjavaapp/SpringJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.springjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
